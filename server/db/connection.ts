035bcd741549b227854888589846488e1b471c38
Jen Hurley <j.s.hurley00@gmail.com>
2023-08-15 11:16:05 +1200

    update connection to knex.default for queries-and-mutations

--- server/db/connection.ts
+++ server/db/connection.ts
@@ -4,6 +4,6 @@ import config from './knexfile.js'
 export type Environment = 'development' | 'test' | 'production'
 const environment = (process.env.NODE_ENV || 'development') as Environment
 
-const connection = knex(config[environment])
+const connection = knex.default(config[environment])
 
 export default connection
